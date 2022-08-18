902dc3c5278bc91131b6ad145d28f5aa8d2dec96
hiratemiyu <miyuhirate@gmail.com>
2022-08-18 14:50:06 +0900

    week6終わり

--- assets-ph1-website-main/js/quiz.js
+++ assets-ph1-website-main/js/quiz.js
@@ -1,56 +1,65 @@
-// let correct = document.getElementById("Q1_correct");
-// //  ＝１個 右側を左側に代入 idを取得するには""または''が必要
-// console.log(correct);
-// // デバック処理の時に使う デバック処理：バグを解決すること
+"use strict" ;
 
-// document.getElementById("Q1_correct").onclick=function(){
-//     document.getElementById("Q1_correctbox").style.display="block";
-// }
+// 3をxに代入する（右から左） const=constant,1つしか代入できない
+// const x = 3 ; 
 
-// /**
-// * 折り畳み表示
-// * 'openHere'というクラスに属するオブジェクトをインライン要素or非表示に変更する。
-// */
-// function openClose(){
-//     var obj = document.getElementsByClassName('openHere');
-//     for(var i=0;i<obj.length;i++){
-//         //非表示ならインライン要素に変更。表示状態なら非表示に変更。
-//         if(obj[i].style.display == "inline-block"){
-//             obj[i].style.display = "none";
-//         }
-//         else{
-//             obj[i].style.display = "inline-block";
-//         }
-//     }
-// }
+// 各問題の中での処理
+// const x = 指示（'.クラス名'）(# id名) (属性）←何も書いてない
+//   allQuiz.forEach(quiz => {
+//     const answers = quiz.querySelectorAll('.js-answer');
+//     const selectedQuiz = Number(quiz.getAttribute('data-quiz'));
+//     const answerBox = quiz.querySelector('.js-answerBox');
+//     const answerTitle = quiz.querySelector('.js-answerTitle');
+//     const answerText = quiz.querySelector('.js-answerText');
 
-/**
-// * 折り畳み表示
-// * 'openHere'というクラスに属するオブジェクトをインライン要素or非表示に変更する。
-// */
-function openClose(){
-    var obj = document.getElementsByClassName('Q1_correctbox');
-    for(var i=0;i<obj.length;i++){
-        //非表示ならインライン要素に変更。表示状態なら非表示に変更。
-        if(obj[i].style.display == "block"){
-            obj[i].style.display = "none";
-        }
-        else{
-            obj[i].style.display = "block";
-        }
-    }
-}
+// 指示するやつ
+// getElementById (Id名（属性そのものではない）id = a)のa の方
+// getElementByClassName（class名）
 
-const list = document.getElementById('list');
-  const btn = document.getElementById('btn');
+// getAttribute(属性（classとか、idとか、data（jsの時に出てくる）とか）)
 
-  btn.addEventListener('click', () => {
+// get系＝取得する https://web-tsuku.life/getattribute/
+// answerの設定方法 cocnstを使って定義 document htmlのid名 属性を取得したら値を返される
 
-  //li要素を生成する
-  const li = document.createElement('li');
-    //li要素にテキストを追加する
-  li.textContent = 'これはリスト要素です';
-    //li要素をul要素内に追加して表示する
-  list.appendChild(li);
+// classは同じ名前を何回も使える（同時多発テロになる）、idでやるとミスが少ない（同時に1発）
 
-  })
+// Q1
+const choice = document.getElementById('Q1_correct');
+const answer = document.getElementById('correctbox');
+const wrongchoice = document.getElementById('Q1_incorrect_1');
+const wronganswer = document.getElementById('incorrectbox');
+
+choice.addEventListener('click', () => {
+    choice.classList.add('is-clicked');
+    answer.classList.add('is-selected');
+    wrongchoice.classList.add('another_is-selected') ;
+    wrongchoice_2.classList.add('another_is-selected') ;
+    // clickしたら表示には関係ない、data-answer = 1 正解、不正解で数字を分けると処理の違いを数字で分けられる 正解→1の処理、不正解→0の処理かも
+    const selectedAnswer = Number(answer.getAttribute('data-answer'));})
+
+wrongchoice.addEventListener('click', () => {
+        wrongchoice.classList.add('is-clicked');
+        wronganswer.classList.add('is-selected');
+        choice.classList.add('another_is-selected') ;
+        wrongchoice_2.classList.add('another_is-selected') ;
+    })
+
+const wrongchoice_2 = document.getElementById('Q1_incorrect_2');
+const wronganswer_2 = document.getElementById('incorrectbox');
+
+    wrongchoice_2.addEventListener('click', () => {
+        wrongchoice_2.classList.add('is-clicked');
+        wronganswer_2.classList.add('is-selected');
+        choice.classList.add('another_is-selected') ;
+        wrongchoice.classList.add('another_is-selected') ;
+    })
+
+// Q2
+    const Q2_choice = document.getElementById('Q2_correct');
+    const Q2_answer = document.getElementById('Q2_correctbox');
+    
+    Q2_choice.addEventListener('click', () => {
+        Q2_answer.classList.add('is-selected');
+        const selectedAnswer = Number(answer.getAttribute('data-answer'));})
+
+    
