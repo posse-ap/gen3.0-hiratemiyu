10c69827372c63cccaa556e0b2b952e70617762f
hiratemiyu <miyuhirate@gmail.com>
2022-11-06 11:37:36 +0900

    棒グラフ

--- script/webapp.js
+++ script/webapp.js
@@ -1,24 +1,82 @@
-$(function () {
-	// 変数に要素を入れる
-	var open = $('.header_btn'),
-		close = $('.modal-close'),
-		container = $('.modal-container');
+const buttonOpen = document.getElementById('modalOpen');
+const modal = document.getElementById('easyModal');
+const buttonClose = document.getElementsByClassName('modalClose')[0];
+const modalBody = document.getElementById('modalbody');
+const completeMark = document.getElementById('completebox');
 
-	//開くボタンをクリックしたらモーダルを表示する
-	open.on('click', function () {
-		container.addClass('active');
-		return false;
-	});
+// ボタンがクリックされた時
+buttonOpen.addEventListener('click', modalOpen);
+function modalOpen() {
+	modal.style.display = 'block';
+	completeMark.style.display = 'none';
+	modalBody.style.display = 'block';
+}
 
-	//閉じるボタンをクリックしたらモーダルを閉じる
-	close.on('click', function () {
-		container.removeClass('active');
-	});
+// バツ印がクリックされた時
+buttonClose.addEventListener('click', modalClose);
+function modalClose() {
+	modal.style.display = 'none';
 
-	//モーダルの外側をクリックしたらモーダルを閉じる
-	$(document).on('click', function (e) {
-		if (!$(e.target).closest('.modal-body').length) {
-			container.removeClass('active');
-		}
-	});
+}
+
+// モーダルコンテンツ以外がクリックされた時
+addEventListener('click', outsideClose);
+function outsideClose(e) {
+	if (e.target == modal) {
+		modal.style.display = 'none';
+	}
+}
+
+$('.spinner-box').hide()
+$('.complete-box').hide()
+
+$('#modal_record').click(function () {
+	$('.modal-body').fadeOut()
+	$('.spinner-box').fadeIn()
+	setTimeout(function () {
+		$('.spinner-box').fadeOut()
+		$('.complete-box').fadeIn()
+	}, 3000);
 });
+
+
+// 棒グラフ
+(function () {
+	'use strict';
+
+	var type = 'bar';
+
+	var data = {
+		labels: [2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30,],
+		datasets: [{
+			data: [0, 2, 4, 6, 8],
+
+		}]
+	};
+
+	var options = {
+		scales: {
+			yAxes: [{
+				ticks: {
+					// min: 0,
+					// max: 400
+					suggestedMin: 0,
+					suggestedMax: 8,
+					stepSize: 2,
+					callback: function (value, index, values) {
+						return  value + 'h' ;
+					}
+				}
+			}]
+		}
+	};
+
+	var ctx = document.getElementById('my_chart').getContext('2d');
+	var myChart = new Chart(ctx, {
+		type: type,
+		data: data,
+		options: options
+	});
+})();
+
+
